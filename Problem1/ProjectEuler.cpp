0402fa37fcfcec7b0e9e70e800752caf650f5ca7
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-04 22:32:35 +0900

    ProjectEulerクラスを追加

---
+++ Problem1/ProjectEuler.cpp
@@ -0,0 +1,9 @@
+//
+//  ProjectEuler.cpp
+//  Problem1
+//
+//  Created by Suzuki Takayuki on 2014/04/04.
+//  Copyright (c) 2014年 T.S. All rights reserved.
+//
+
+#include "ProjectEuler.h"
---
+++ Problem1/ProjectEuler.h
@@ -0,0 +1,14 @@
+//
+//  ProjectEuler.h
+//  Problem1
+//
+//  Created by Suzuki Takayuki on 2014/04/04.
+//  Copyright (c) 2014年 T.S. All rights reserved.
+//
+
+#ifndef __Problem1__ProjectEuler__
+#define __Problem1__ProjectEuler__
+
+#include <iostream>
+
+#endif /* defined(__Problem1__ProjectEuler__) */
