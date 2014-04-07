716a382774e81df3ef50e91557647dd4441bf651
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-07 15:00:27 +0900

    p1のif文を修正

---
+++ Problem2/Problem2/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Problem2
+//
+//  Created by Suzuki Takayuki on 2014/04/07.
+//  Copyright (c) 2014年 T.S. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
