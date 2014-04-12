3f4a73bd0d6454ebed3a8071ea50faffbdfe17ca
kabuki0111 <hikari_to_karasu@hotmail.co.jp>
2014-04-12 14:50:09 +0900

    XCTestを導入

---
+++ Problem2/Problem2 Tests/Problem2_Tests.mm
@@ -0,0 +1,36 @@
+//
+//  Problem2_Tests.m
+//  Problem2 Tests
+//
+//  Created by Suzuki Takayuki on 2014/04/12.
+//  Copyright (c) 2014年 T.S. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#include "ProjectEuler.h"
+
+@interface Problem2_Tests : XCTestCase
+
+@end
+
+@implementation Problem2_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    ProjectEuler *euler = new ProjectEuler;
+    //XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
